185fd72cfec7d21ebf1bcf10598911b96cb93213
haeeunwoo-crypto <haeeun.woo@day1company.co.kr>
2026-03-17 15:52:04 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'], // 한국어 폰트 적용
+      }
+    },
+  },
+  plugins: [],
+}
